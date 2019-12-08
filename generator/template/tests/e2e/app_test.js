c0685ad1e82f82929c5e064502d049c5c5b1cc50
Davert <davert.ua@gmail.com>
2019-12-09 01:39:53 +0200

    improved a test to make it better

--- generator/template/tests/e2e/app_test.js
+++ generator/template/tests/e2e/app_test.js
@@ -2,6 +2,8 @@ Feature('App');
 
 Scenario('Open app', (I) => {
   I.amOnPage('/');
+
+  I.say('This test will pass if you enabled demo component');
   I.see('Demo Component', 'h1');
   I.fillField('First name', 'tester'); // using semantic locators
   I.fillField('Last name', 'end2end');
@@ -11,5 +13,6 @@ Scenario('Open app', (I) => {
   I.click('Submit');
   I.see('"first_name":"tester"','#result');
   I.see('"last_name":"end2end"','#result');
+  I.say("Now, it's your turn! Use this pause mode to write test on your own!");
   pause(); // TODO: Add more assertions when paused!
 });
